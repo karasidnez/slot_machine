724aab9bcbf29ccb09361a43bf8edd336a561963
karasidnez <karasdnez@gmail.com>
2026-05-10 21:05:04 +0300

    main

--- student_card/lib/main.dart
+++ student_card/lib/main.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
+import 'name_widget.dart';
+import 'group_widget.dart';
+import 'language_widget.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Визитка студента',
+      theme: ThemeData.dark(),
+      home: const BusinessCardScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BusinessCardScreen extends StatelessWidget {
+  const BusinessCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xFF1E3C72), Color(0xFF2A5298), Color(0xFF6B8DD6)],
+            stops: [0.0, 0.5, 1.0],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              NameWidget(),
+              SizedBox(height: 20),
+              GroupWidget(),
+              SizedBox(height: 20),
+              LanguageWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
